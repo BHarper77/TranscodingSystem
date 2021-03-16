232e395b102d17d443b389b4ba6848035477ba07
1700485 <1700485@uad.ac.uk>
2021-03-16 16:55:40 +0000

    Fixed send.js not being able to connect to K8s cluster

--- send.js
+++ send.js
@@ -1,10 +1,9 @@
 const amqp = require('amqplib/callback_api');
 
 const localHost = "amqp://127.0.0.1:5672";
-const cluster = "amqp://rabbitmq-cluster-default-user:dJhLl2aVF78Gn07g2yGoRuwjXSc6tT11@192.168.49.2:30861"; //for amqp
-const cluster2 = "amqp://192.168.49.2:30861"; //for http
+const cluster = "amqp://EmZn4ScuOPLEU1CGIsFKOaQSCQdjhzca:dJhLl2aVF78Gn07g2yGoRuwjXSc6tT11@192.168.49.2:30861"; //for amqp
 
-amqp.connect(cluster2, function(error0, connection) 
+amqp.connect(cluster, function(error0, connection) 
 {
     if (error0)
     {
