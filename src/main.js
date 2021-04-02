b76791b0339d696a235e0aa20160c4341815b314
BHarper7 <bradyharper11@googlemail.com>
2021-04-02 15:20:00 +0100

    Added basic grid styling

--- src/main.js
+++ src/main.js
@@ -4,13 +4,7 @@ const app = express();
 const http = require("http").createServer(app);
 const io = require("socket.io")(http);
 
-app.get("/", (req, res) => {
-    res.sendFile(__dirname + "/index.html");
-});
-
-app.get("/upload.js", (req, res) => {
-    res.sendFile(__dirname + "/upload.js");
-});
+app.use(express.static("../src/"));
 
 const port = 8080;
 http.listen(port, () => console.log(`Server is running on port ${port}`));
