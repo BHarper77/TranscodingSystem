714d67c3339b1158f7fd0fb0e6643e12280c1b89
1700485 <1700485@uad.ac.uk>
2021-04-29 14:59:41 +0100

    Minor changes

--- scalabilityTesting/send.js
+++ scalabilityTesting/send.js
@@ -15,7 +15,7 @@ amqp.connect(cluster, (error0, connection) => {
             durable: true
         });
 
-        for (i = 0; i < 50; i++)
+        for (i = 0; i < 3; i++)
         {
             let msg = "message " + i;
             channel.sendToQueue(queue, Buffer.from(msg));
